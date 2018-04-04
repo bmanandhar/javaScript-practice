374de6874e153d077ce4a968afb9b147d5f3680b
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-04 15:52:47 -0700

    examples of types of data in JavaScript

---
+++ typeof.js
@@ -0,0 +1,9 @@
+//Types of data in JavaScipt
+//Boolean, Null, Undefined, Number, String, Symbol
+// and Object
+console.log((typeof 9) == 'number')
+console.log((typeof 'abc') == 'string')
+console.log((typeof true) == 'boolean')
+console.log((typeof false) == 'boolean')
+console.log((typeof null) == 'object')
+console.log((typeof true) == 'boolean')
