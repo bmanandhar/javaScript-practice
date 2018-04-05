2acac1e17149f9a808b7afba1f3facaed570e57b
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-04 17:35:49 -0700

    examples how parenthese are important

---
+++ parentheses_in_syntax.js
@@ -0,0 +1,5 @@
+var iData = 99;
+
+typeof iData + ' Wisen'; // It will return 'number Wisen'
+typeof (iData + ' Wisen') === 'string'; // It will return 'string'
+console.log((99 + 'abc') === '99abc') // surpsing fact
